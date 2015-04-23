89fe728db8cb2c4aeeb8120af5708b93d1ed1d91
Gerard Kanters <gkanters@gmail.com>
2015-04-23 07:40:32 +0200

    Correct a href

--- views/default/page/elements/search.php
+++ views/default/page/elements/search.php
@@ -4,8 +4,8 @@ $action = elgg_get_site_url() ."videos/youtube";
 
 $htmlBody = <<<END
 <a href="/videos/all" title="Watch videos on $site_name "> 
-<img alt="Videos on '. $site_name . ' " height="36" src="/_graphics/elgg_logo.png" width="36" align="left"></a>                  
-<img alt="Youtube Logo" height="36" src="/mod/videos/graphics/youtube.jpg" width="36" align="right"></a>
+<img alt="Videos on '. $site_name . ' " height="36" src="/_graphics/elgg_logo.png" width="36" align="left"></a>                 
+<a href="https://www.youtube.com" title="Powered by Youtube"><img alt="Youtube Logo" height="36" src="/mod/videos/graphics/youtube.jpg" width="36" align="right"></a>
 <br>
 <form action="$action" method="GET">
   <div>
